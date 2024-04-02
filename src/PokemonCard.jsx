5a98049aaccae7381a856b3ca5f496a6f7c6385c
lucasA49 <lucasaksu@gmail.com>
2024-04-02 23:00:12 +0200

    quete 5

--- src/PokemonCard.jsx
+++ src/PokemonCard.jsx
@@ -1,6 +1,6 @@
+import PropTypes from 'prop-types';
 function PokemonCard({pokemon}) {
-
-  //  console.log(props)
+   console.log(pokemon)
 
 
   return (<>
@@ -10,6 +10,12 @@ function PokemonCard({pokemon}) {
     <p>{pokemon.name}</p>
   </>
 
-  )
-}
+      )
+  }
+  PokemonCard.propTypes = {
+  pokemon: PropTypes.shape({
+    imgSrc: PropTypes.string,
+    name: PropTypes.string.isRequired
+  })
+  };
 export default PokemonCard;
